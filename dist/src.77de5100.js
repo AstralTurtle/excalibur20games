eae97c63554dfa3150fc59ebeee99068e2a64750
AstralTurtle <jamesacs329@gmail.com>
2024-07-14 16:04:35 -0400

    rename files

--- dist/src.77de5100.js
+++ dist/src.77de5100.js
@@ -34141,7 +34141,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64121" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64412" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
